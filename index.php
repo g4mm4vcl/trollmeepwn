0dc3f2f0f790e0fa33440e0e079ecb4c70f83ed6
g4mm4vcl <39906610+g4mm4vcl@users.noreply.github.com>
2018-06-16 17:17:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "g4mm4 is my hero";
-?>
+<!DOCTYPE html>
+<html>
+<title>
+CNN International - World news - breaking news, video, headlines and opinion
+</title>
+<body>
+
+<p>
+<a href="https://tradahacking.vn/lu%E1%BA%ADt-an-ninh-m%E1%BA%A1ng-v%C3%A0-nh%E1%BB%AFng-h%E1%BB%87-l%E1%BB%A5y-87f334b28bff">
+<img border="0" alt="breaking news" src="link.png" style='height: 100%; width: 100%; object-fit: contain'>
+</a>
+</p>
+
+</body>
+</html>
