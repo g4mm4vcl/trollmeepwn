1341ca77efa90e6d26032f1aa998fb97c7a93aed
g4mm4vcl <39906610+g4mm4vcl@users.noreply.github.com>
2019-05-08 17:02:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <title>
-CNN International - World news - breaking news, video, headlines and opinion
+Subdomain Takeover - Hijacked by g4mm4
 </title>
 <body>
 
 <p>
 <a href="https://tradahacking.vn/lu%E1%BA%ADt-an-ninh-m%E1%BA%A1ng-v%C3%A0-nh%E1%BB%AFng-h%E1%BB%87-l%E1%BB%A5y-87f334b28bff">
-<img border="0" alt="breaking news" src="link1.png" style='height: 100%; width: 100%; object-fit: contain'>
+<img border="0" alt="breaking news" src="linkxxx.png" style='height: 100%; width: 100%; object-fit: contain'>
 </a>
 </p>
 
